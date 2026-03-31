9a670d6b2a1095b35563389f00ef1d93a1410159
Sara Costa Cabral Mululo <saramululo@gmail.com>
2026-03-31 15:51:20 -0500

    Replace MockitoAnnotation with @ExtendWith(MockitoExtension.class)

--- src/test/java/service/PackServiceTest.java
+++ src/test/java/service/PackServiceTest.java
@@ -3,21 +3,22 @@ package service;
 import com.sara.unittestingpractice.entity.Pack;
 import com.sara.unittestingpractice.repository.PackRepository;
 import com.sara.unittestingpractice.service.PackService;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
 import static org.mockito.ArgumentMatchers.any;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+@ExtendWith(MockitoExtension.class)
 public class PackServiceTest {
 
   @Mock
@@ -27,29 +28,19 @@ public class PackServiceTest {
   PackService packService;
 
   Pack pack;
-  private AutoCloseable mocks;
-
 
   @BeforeEach
   void setUp(){
-    mocks=MockitoAnnotations.openMocks(this);
-    pack = new Pack();
-    pack.setStatus("IN_RETAILER");
-    pack.setId(1L);
-  }
-
-  @AfterEach
-  void tearDown() throws Exception {
-    mocks.close();
+    pack = new Pack(1L,"IN_RETAILER", 100L);
   }
 
   // --- Happy Path ---
   @Test
   void activatedPack_shouldSetStatusToActivated() {
-    when(packRepository.findById(pack.getId())).thenReturn(Optional.of(pack));
+    when(packRepository.findById(pack.getPackId())).thenReturn(Optional.of(pack));
     when(packRepository.save(any(Pack.class))).thenReturn(pack);
 
-    Pack result = packService.activatePackById(pack.getId());
+    Pack result = packService.activatePackById(pack.getPackId());
     assertEquals("ACTIVATED", result.getStatus());
     verify(packRepository).save(pack);
   }
@@ -64,19 +55,19 @@ public class PackServiceTest {
 
   void activatePack_shouldThrowForInvalidStatuses(String status) {
     pack.setStatus(status);
-    when(packRepository.findById(pack.getId())).thenReturn(Optional.of(pack));
-    Exception exception = assertThrows(IllegalArgumentException.class, ()->packService.activatePackById(pack.getId()));
-    assertEquals("com.sara.unittestingpractice.entity.Pack must be in retailer to be activated",
+    when(packRepository.findById(pack.getPackId())).thenReturn(Optional.of(pack));
+    Exception exception = assertThrows(IllegalArgumentException.class, ()->packService.activatePackById(pack.getPackId()));
+    assertEquals("Pack must be in retailer to be activated",
         exception.getMessage());
     verify(packRepository, never()).save(any());
   }
 
-  // --- com.sara.unittestingpractice.entity.Pack Not Found ---
+  // --- Pack Not Found ---
   @Test
   void activatePack_shouldThrowIfPackNotFound(){
     when(packRepository.findById(999L)).thenReturn(Optional.empty());
     Exception exception = assertThrows(java.util.NoSuchElementException.class,
         () -> packService.activatePackById(999L));
-    assertEquals("com.sara.unittestingpractice.entity.Pack not found with id: 999", exception.getMessage());
+    assertEquals("Pack not found with id: 999", exception.getMessage());
   }
 }
