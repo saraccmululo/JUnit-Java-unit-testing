8e7a874950becfd1107b43ff5ee6af85a4f000be
Sara Costa Cabral Mululo <saramululo@gmail.com>
2026-04-24 13:42:54 -0500

    refactor: update PackService to use PackStatus enum instead of String type

--- src/main/java/com/sara/unittestingpractice/service/PackService.java
+++ src/main/java/com/sara/unittestingpractice/service/PackService.java
@@ -1,6 +1,7 @@
 package com.sara.unittestingpractice.service;
 
 import com.sara.unittestingpractice.entity.Pack;
+import com.sara.unittestingpractice.entity.PackStatus;
 import com.sara.unittestingpractice.repository.PackRepository;
 import org.springframework.stereotype.Service;
 
@@ -23,16 +24,15 @@ public class PackService {
     Pack pack = packRepository.findById(id)
         .orElseThrow(()-> new NoSuchElementException("Pack not found with id: " + id));
 
-
-    if ("ACTIVATED".equals(pack.getStatus())) { //check if pa ck was already activated
+    if (pack.getStatus() == PackStatus.ACTIVATED) { //check if pack was already activated
       throw new IllegalStateException("Pack is already activated");
     }
 
-    if(!"IN_RETAILER".equals(pack.getStatus())){ //check if status is not "IN_RETAILER"
-      throw new IllegalArgumentException("Pack must be in retailer to be activated");
+    if(pack.getStatus() != PackStatus.IN_RETAILER){ //check if status is not "IN_RETAILER"
+      throw new IllegalStateException("Pack must be in retailer to be activated");
     }
 
-    pack.setStatus("ACTIVATED"); //change status to activated
+    pack.setStatus(PackStatus.ACTIVATED); //change status to activated
     return packRepository.save(pack);
   } //saving using the repository and returning pack
 
